000ec0e19932af8c348352369296449c7a1577c6
Bas Visscher <bas.visscher24@gmail.com>
2023-02-16 18:32:32 +0100

    Added class

---
+++ Editor/Utils/AutoUpdator.cs
@@ -0,0 +1,36 @@
+﻿#if (UNITY_EDITOR)
+
+using Codice.CM.Client.Differences.Graphic;
+using Codice.CM.Common;
+using NUnit.Framework.Constraints;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Wakatime
+{
+    public class AutoUpdator
+    {
+        private Logger Logger { get; }
+        private Settings Settings { get; set; }
+        public AutoUpdator(Logger logger, Settings settings)
+        {
+            Logger = logger;
+            Settings = settings;
+        }
+
+
+
+    }
+        
+}
+
+
+#endif
